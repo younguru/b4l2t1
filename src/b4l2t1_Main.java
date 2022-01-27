8a33d981ab9a9ff637adab23382fa50fccadca0d
Kirill Makarochkin <kiosun@mail.ru>
2022-01-27 16:47:33 +0300

    1

---
+++ src/b4l2t1_Main.java
@@ -0,0 +1,13 @@
+public class b4l2t1_Main {
+    public static void main(String[] args) {
+        int[] intArray = new int[]{1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        String result;
+        for (int i = 0; i < intArray.length; i++) {
+            if (intArray[i]>0) {
+                if(intArray[i] % 2 == 0) {
+                    
+                }
+            }
+        }
+    }
+}
